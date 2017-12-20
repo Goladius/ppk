0b490cb15bb5ab66376f51cde59df943fb9aaa12
Talut Taşgıran <talut@tasgiran>
2017-12-20 16:15:51 +0300

    PublicPrivateKey-ppk

---
+++ ppk.go
@@ -0,0 +1,49 @@
+package ppk
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"os"
+	"encoding/pem"
+	"crypto/x509"
+	"crypto/rand"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+)
+
+func GetPrivateKey() *rsa.PrivateKey {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Println(err.Error)
+	}
+	return privateKey
+}
+// Saving private key with given name..
+func SavePrivateKey(fileName string, key *rsa.PrivateKey) {
+	outFile, err := os.Create(fileName)
+	checkError(err)
+	defer outFile.Close()
+
+	var privateKey = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+
+	err = pem.Encode(outFile, privateKey)
+	checkError(err)
+}
+
+// Saving public key with given name..
+func SavePublicKey(fileName string, pubKey *rsa.PublicKey) {
+	pub, _ := ssh.NewPublicKey(pubKey)
+	ioutil.WriteFile(fileName+".pub", ssh.MarshalAuthorizedKey(pub), 0777)
+}
+
+
+// check error
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
---
+++ ppk_test.go
@@ -0,0 +1,17 @@
+package ppk
+
+import (
+	"testing"
+)
+
+func TestGetPrivateKey(t *testing.T) {
+
+}
+
+func TestSavePrivateKey(t *testing.T) {
+
+}
+
+func TestSavePublicKey(t *testing.T) {
+	
+}
