932ac9d0fbd8ec6e8c50983b36525218df72f885
Talut TAŞGIRAN <iletisim@taluttasgiran.com.tr>
2018-02-08 11:26:56 +0300

    error handling

--- ppk.go
+++ ppk.go
@@ -46,6 +46,6 @@ func SavePublicKey(fileName string, privateKey *rsa.PrivateKey) {
 func checkError(err error) {
 	if err != nil {
 		fmt.Println("Fatal error ", err.Error())
-		os.Exit(1)
 	}
+}
 }
